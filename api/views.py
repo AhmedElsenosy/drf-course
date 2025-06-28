e59dc2837c24328e045abe20c64047e050d40b89
AhmedElsenosy <ahmedelsenosy487@gmail.com>
2025-06-28 14:28:06 +0300

    Add serializers, views, and URLs for product management in the API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Product, Order, OrderItem
+from .serializers import ProductSerializer
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+
+    return Response(serializer.data)
