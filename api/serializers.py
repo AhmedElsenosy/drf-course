7a797e348d78d03d757f62721cf1b4251be9a0dc
AhmedElsenosy <ahmedelsenosy487@gmail.com>
2025-06-28 16:09:13 +0300

    Enhance API with order management: update serializers, views, and URLs for Order and OrderItem

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,6 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = (
             'name',
-            'description',
             'price',
             'stock'
         )
@@ -16,3 +15,37 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError('Price must be greater than 0.')
         return value
+    
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    # product = ProductSerializer()
+    product_name = serializers.CharField(source = 'product.name')
+    product_price = serializers.DecimalField(source = 'product.price', max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal',
+        )
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many = True, read_only = True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'items',
+            'total_price',
+        )
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from .models import Product, Order, OrderItem
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -17,5 +17,12 @@ def product_list(request):
 def product_details(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
 
     return Response(serializer.data)
